3f44fa65871955fde68c4a05df6d28a5280d7ab8
rho13 <rogiersaxion@gmail.com>
2015-11-24 10:27:09 +0100

    fixed buvg on release branch

--- Branchtest.java
+++ Branchtest.java
@@ -1,5 +1,5 @@
 public class Branchtest {
 	public static void main(String [] args) {
-		System.out.println("Hello from master (changed by pcB)");
+		System.out.println("Hello from release branch");
 	}
 }

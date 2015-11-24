5b17626427436bab6d7bd0dc5477c9dddb092799
rho13 <rogiersaxion@gmail.com>
2015-11-24 11:31:59 +0100

    update

--- Branchtest.java
+++ Branchtest.java
@@ -2,5 +2,6 @@ public class Branchtest {
 	public static void main(String [] args) {
 		System.out.println("Hello from master..Updated (changed by pcB)");
 		System.out.println("Hello from release branch");
+//added from someNewBranch
 	}
 }

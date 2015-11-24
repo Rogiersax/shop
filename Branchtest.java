4c6b48732989dc395c739c63be2affec59f7479c
rho13 <rogiersaxion@gmail.com>
2015-11-24 10:50:30 +0100

    update

--- Branchtest.java
+++ Branchtest.java
@@ -1,5 +1,6 @@
 public class Branchtest {
 	public static void main(String [] args) {
-		System.out.println("Hello from release branch");
+		
+\\System.out.println("Hello from release branch");
 	}
 }
